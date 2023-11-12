a9826b3b4d90e258500847342b3c732246cc02c5
CarlosMario123 <221220@ids.upchiapas.edu.mx>
2023-11-12 13:32:49 -0600

    useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,36 +1,42 @@
-import React, { useState } from "react";
-import { useFetch } from "../hooks/useFetch";
+import { useEffect, useState } from "react";
 
-export default function PostmanLike() {
-  const { state, setUrl } = useFetch("");
-  const [endpoint, setEndpoint] = useState("");
 
-  const realizarPeticion = () => {
-    setUrl(endpoint);
-  };
+export const useFetch = ( url ) => {
 
-  const handleInputChange = (event) => {
-    setEndpoint(event.target.value);
-  };
+    const [state, setState] = useState({
+        data: null,
+        isLoading: true,
+        hasError: null,
+    })
 
-  return (
-    <div className="flex flex-col items-center p-3 bg-gray-200 rounded-md gap-y-4 w-96">
-      <h2 className="w-full">Ingrese el endpoint para realizar la petición</h2>
-      <input type="text" className="w-full" value={endpoint} onChange={handleInputChange} />
-      <button className="w-56 p-1 text-white bg-blue-800 hover:bg-blue-500" onClick={realizarPeticion}>
-        Realizar Petición
-      </button>
 
-      <div style={{ maxHeight: "48px", overflowY: "auto" }} className="w-full">
-        <p>Respuesta:</p>
-        {state.isLoading ? (
-          <p>Cargando...</p>
-        ) : state.hasError ? (
-          <p>Error en la petición: {state.hasError.message}</p>
-        ) : (
-          <pre>{JSON.stringify(state.data, null, 2)}</pre>
-        )}
-      </div>
-    </div>
-  );
+    const getFetch = async () => {
+
+        setState({
+            ...state,
+            isLoading: true,
+        });
+
+        const resp = await fetch(url);
+        const data = await resp.json();
+
+        setState({
+            data,
+            isLoading: false,
+            hasError: null,
+        });
+    }
+
+
+    useEffect(() => {
+        getFetch();
+    }, [url])
+    
+
+
+    return {
+        data:      state.data,
+        isLoading: state.isLoading,
+        hasError:  state.hasError,
+    };
 }
